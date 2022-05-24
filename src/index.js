8bf7edfc6ab2b7cf07d7ec1a84326d9f7d9a05dd
marquisalexander <marquis.alexander@outlook.com>
2022-05-24 14:26:03 -0300

    feature: deposit and withdraw funds

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ function verifyIsExistsAccountCPF(request, response, next) {
 	return next();
 }
 
+function getBalance(statement) {
+	const balance = statement.reduce((acc, operator) => {
+		if (operator.type === "credit") {
+			return acc + operator.amount;
+		} else {
+			return acc - operator.amount;
+		}
+	}, 0);
+
+	return balance;
+}
+
 app.use(express.json());
 
 app.post("/account", (request, response) => {
@@ -47,6 +59,44 @@ app.get("/statement", verifyIsExistsAccountCPF, (request, response) => {
 	const { customer } = request;
 
 	return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIsExistsAccountCPF, (request, response) => {
+	const { description, amount } = request.body;
+
+	const { customer } = request;
+
+	const statementOperation = {
+		description,
+		amount,
+		create_at: new Date(),
+		type: "credit",
+	};
+
+	customer.statement.push(statementOperation);
+
+	return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIsExistsAccountCPF, (request, response) => {
+	const { amount } = request.body;
+	const { customer } = request;
+
+	const balance = getBalance(customer.statement);
+
+	if (balance < amount) {
+		return response.status(400).json({ error: "Insufficient funds!" });
+	}
+
+	const statementOperation = {
+		amount,
+		create_at: new Date(),
+		type: "debit",
+	};
+
+	customer.statement.push(statementOperation);
+
+	return response.status(201).send();
 });
 
 app.listen(3333);
